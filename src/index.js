bc140b9c28fa6b3dfdbc0b0b23886fa71801f529
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-14 23:47:49 +0330

    Add Message component in index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const Person = () => <h1>This Hamid</h1>;
+const Message = () => {
+  return (
+    <p>
+      We are leaning <strong>React</strong>
+    </p>
+  );
+};
 
 function Greeting() {
   return (
     <div>
       <Person />
-      <p>Just start to teach react</p>
+      <Message />
     </div>
   );
 }
