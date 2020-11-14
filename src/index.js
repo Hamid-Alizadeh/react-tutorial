01ca76be208ee836da163da89d4fa540f059bc70
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-14 23:42:51 +0330

    Add new nested componnent in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+const Person = () => <h1>This Hamid</h1>;
+
 function Greeting() {
-  return <h1>This Hamid</h1>;
+  return (
+    <div>
+      <Person />
+      <p>Just start to teach react</p>
+    </div>
+  );
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
