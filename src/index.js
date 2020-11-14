668844b98177997f3fa094eede4a8606e3d6b167
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-15 03:05:38 +0330

    Create bookList array of book objects to load data dynamicaly

--- src/index.js
+++ src/index.js
@@ -4,28 +4,40 @@ import ReactDom from "react-dom";
 // CSS
 import "./index.css";
 
-const firstBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
-  title: "A Promise Land",
-  author: "Barack Obama",
-};
-
-const secondBook = {
-  img:
-    "https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg",
-  title: "Diary of a Wimpy Kid - The Deep End",
-  author: "Jeff Kinney",
-};
+const bookList = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+    title: "A Promise Land",
+    author: "Barack Obama",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg",
+    title: "Diary of a Wimpy Kid - The Deep End",
+    author: "Jeff Kinney",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg",
+    title: "Greenlights",
+    author: "Matthew McConaughey",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81hx39H8JEL._AC_UL200_SR200,200_.jpg",
+    title: "The World Needs Who You Were Made to Be",
+    author: "Joanna Gaines",
+  },
+];
 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <BookImage img={img} alt={title} />
       <Title title={title} />
       <Author author={author} />
-      {props.children}
     </article>
   );
 };
@@ -38,31 +50,9 @@ const Author = (props) => <strong>{props.author.toUpperCase()}</strong>;
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-          quaerat voluptate harum exercitationem.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {bookList.map((book) => {
+        return <Book book={book} />;
+      })}
     </section>
   );
 }
