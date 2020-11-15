ba116dc121e2ddddb33f3b545f435b4e45b88d46
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2020-11-15 20:47:17 +0330

    Add list of books as bookList const in books.js

---
+++ src/books.js
@@ -0,0 +1,30 @@
+export const bookList = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+    title: "A Promise Land",
+    author: "Barack Obama",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg",
+    title: "Diary of a Wimpy Kid - The Deep End",
+    author: "Jeff Kinney",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg",
+    title: "Greenlights",
+    author: "Matthew McConaughey",
+  },
+  {
+    id: 4,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81hx39H8JEL._AC_UL200_SR200,200_.jpg",
+    title: "The World Needs Who You Were Made to Be",
+    author: "Joanna Gaines",
+  },
+];
